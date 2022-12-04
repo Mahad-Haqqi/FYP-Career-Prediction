4f3ea1158a9b8af615c7e9293db2ce2b1c83a107
Mahad-Haqqi <56938790+Mahad-Haqqi@users.noreply.github.com>
2022-12-04 19:31:43 +0000

    result percentages

--- client/src/components/Result/Result.js
+++ client/src/components/Result/Result.js
@@ -1,19 +1,66 @@
-import React from "react"
-import "./result.css"
-const Result = ({type}) => {
-  return (
-    <div className="result1">
-      <h1>Based On the Performance of Test</h1>
-      <h2>Our Prediction is</h2>
-      <h1>{type}</h1>
-      <p>
-        This personality type tends to prefer working in groups, especially in
-        roles where they care for the well-being of others. Good careers for
-        those with the ESFJ personality type are social worker, nurse or
-        healthcare worker, sales representative, or in public relations.
-      </p>
-    </div>
-  )
-}
+import React, { useState } from "react";
+import { Doughnut } from "react-chartjs-2";
+import "./result.css";
+import { personality } from "./data";
 
-export default Result
+const Result = ({ type, first, second, third, fourth }) => {
+  const chartData1 = {
+    labels: ["Extroversion", "Introversion"],
+    datasets: [
+      {
+        data: [first.E, first.I],
+        backgroundColor: ["red", "blue"],
+      },
+    ],
+  };
+  const chartData2 = {
+    labels: ["Intuative", "sensing"],
+    datasets: [
+      {
+        data: [second.N, second.S],
+        backgroundColor: ["red", "blue"],
+      },
+    ],
+  };
+  const chartData3 = {
+    labels: ["Feeling", "Thinking"],
+    datasets: [
+      {
+        data: [third.F, third.T],
+        backgroundColor: ["red", "blue"],
+      },
+    ],
+  };
+  const chartData4 = {
+    labels: ["Judging", "Perceiveing"],
+    datasets: [
+      {
+        data: [fourth.J, fourth.P],
+        backgroundColor: ["red", "blue"],
+      },
+    ],
+  };
+
+  console.log("data", personality[type.type]);
+  return (
+    <>
+      <div className="result1">
+        <h1>Based On the Performance of Test</h1>
+        <h2>Our Prediction is</h2>
+        <h1>
+          {type.type}
+          <span>({personality[type.type][0]})</span>
+        </h1>
+        <p>{personality[type.type][1]}</p>
+      </div>
+      <div>
+        <Doughnut data={chartData1} />
+        <Doughnut data={chartData2} />
+        <Doughnut data={chartData3} />
+        <Doughnut data={chartData4} />
+      </div>
+    </>
+  );
+};
+
+export default Result;
